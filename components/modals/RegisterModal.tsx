6fd6f4e8320c1dfbfa9d501a14a6b9d1eeefbd44
jon-jc <jonathancho.jc@gmail.com>
2024-03-12 14:13:40 -0700

    feat: added login ui
    Add ToasterProvider and Google login option in RegisterModal

--- components/modals/RegisterModal.tsx
+++ components/modals/RegisterModal.tsx
@@ -13,6 +13,11 @@ import { errorMonitor } from "events";
 import Modal from "./Modal";
 import Heading from "../Heading";
 import Input from "../inputs/Input";
+import toast from "react-hot-toast";
+import Button from "../Button";
+import { BsGoogle } from "react-icons/bs";
+import { GrGoogle } from "react-icons/gr";
+import { FcGoogle } from "react-icons/fc";
 
 const RegisterModal = () => {
   const registerModal = useRegisterModal();
@@ -38,7 +43,7 @@ const RegisterModal = () => {
         registerModal.onClose();
       })
       .catch((error) => {
-        console.log(error);
+        toast.error("An error occurred, please try again later");
       })
       .finally(() => {
         setIsLoading(false);
@@ -78,6 +83,43 @@ const RegisterModal = () => {
     </div>
   );
 
+  const footerContent = (
+    <div className="flex flex-col gap-4 mt-3">
+      <hr />
+      <Button
+        outline
+        label="Continue with Google"
+        icon={FcGoogle}
+        onClick={() => {}}
+      />
+      <Button
+        outline
+        label="Continue with GitHub"
+        icon={AiFillGithub}
+        onClick={() => {}}
+      />
+      <div
+        className="
+      text-slate-500
+      text-center
+      mt-4
+      font-light"
+      >
+        <div className="flex flex-row items-center gap-2 justify-center">
+          <div>Already have an account?</div>
+          <div
+            onClick={registerModal.onClose}
+            className="
+          text-slate-900
+          cursor-pointer
+          hover:underline"
+          >
+            Login
+          </div>
+        </div>
+      </div>
+    </div>
+  );
   return (
     <Modal
       disabled={isLoading}
@@ -87,6 +129,7 @@ const RegisterModal = () => {
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
+      footer={footerContent}
     />
   );
 };
